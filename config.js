f1c1b1b367b5f5f88cedbcbab3ca9e891d751ede
Krzysztof Stankiewicz <k.stankiewicz@outlook.de>
2026-09-15 14:52:12 +0200

    subbase

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: ""
+  SUPABASE_URL: "https://palftjilodyawbhooexo.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_xO1WRNNXntnLzF5vDkGU-Q_USqTGhW1"
 };
